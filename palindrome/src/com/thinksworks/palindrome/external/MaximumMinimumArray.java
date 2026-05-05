26ddcefcca4ba7dc0ad06a92efa123006d16164f
rudresh <rudreshgowdahp@gmail.com>
2026-05-05 08:40:42 +0530

    update

---
+++ palindrome/src/com/thinksworks/palindrome/external/MaximumMinimumArray.java
@@ -0,0 +1,22 @@
+package com.thinksworks.palindrome.external;
+
+public class MaximumMinimumArray {
+    public static void main(String[] args) {
+        int[] arr = {3, 7, 1, 9, 2};
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Minimum: " + min);
+        System.out.println("Maximum: " + max);
+    }
+}
--- palindrome/src/com/thinksworks/palindrome/external/NumberCheck.java
+++ palindrome/src/com/thinksworks/palindrome/external/NumberCheck.java
@@ -2,8 +2,8 @@ package com.thinksworks.palindrome.external;
 
 public class NumberCheck {
     public static void main(String[] args) {
-        int number = 25;
-        if (number %2 == 0 ){
+        int number = -26;
+        if (number % 2 == 0 ){
             System.out.println("even");
         }else {
             System.out.println("odd");
