1e84f1c7f7c07ade5d8b01dda5102c878c7ffefe
rudresh <rudreshgowdahp@gmail.com>
2026-05-15 21:01:15 +0530

    update pattern printing  in java

---
+++ palindrome/src/com/thinksworks/palindrome/external/DiamondPattern.java
@@ -0,0 +1,28 @@
+package com.thinksworks.palindrome.external;
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n + i - 1; j++) {
+                if (j <= n - i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n + i - 1; j++) {
+                if (j <= n - i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+            }
+        }
+
