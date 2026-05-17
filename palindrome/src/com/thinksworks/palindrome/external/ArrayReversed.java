dee22a018a9900a7593dec9614631fb155d2a391
rudresh <rudreshgowdahp@gmail.com>
2026-05-17 07:41:26 +0530

    update pattern printing  in java

--- palindrome/src/com/thinksworks/palindrome/external/ArrayReversed.java
+++ palindrome/src/com/thinksworks/palindrome/external/ArrayReversed.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class ArrayReversed {
     public static void main(String[] args) {
-        int arr[] ={1,2,3,4,5,6};
+        int arr[] ={1,2,3,4,5,6,7};
         for (int i =0; i<arr.length/2; i++){
             int t =arr[i];
             arr[i]=arr[arr.length-1-i];
