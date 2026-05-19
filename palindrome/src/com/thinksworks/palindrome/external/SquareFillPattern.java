88de99e034cf3a2b84a19411bb6929d9b488deea
rudresh <rudreshgowdahp@gmail.com>
2026-05-19 21:54:08 +0530

    update pattern printing  in java

--- palindrome/src/com/thinksworks/palindrome/external/SquareFillPattern.java
+++ palindrome/src/com/thinksworks/palindrome/external/SquareFillPattern.java
@@ -2,7 +2,7 @@ package com.thinksworks.palindrome.external;
 
 public class SquareFillPattern {
     public static void main(String[] args) {
-        for( int i=1;i<=6;i++){
+        for( int i=1;i<=8;i++){
             for (int j =1;j<=6;j++){
                 System.out.print("*"+" ");
             }
