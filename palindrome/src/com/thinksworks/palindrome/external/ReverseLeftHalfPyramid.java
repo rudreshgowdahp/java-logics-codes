764f22f21ffd444e9b518ecfcfb74cce0e59403d
rudresh <rudreshgowdahp@gmail.com>
2026-05-21 16:04:53 +0530

    update pattern printing  in java

---
+++ palindrome/src/com/thinksworks/palindrome/external/ReverseLeftHalfPyramid.java
@@ -0,0 +1,17 @@
+package com.thinksworks.palindrome.external;
+
+public class ReverseLeftHalfPyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n; j++) {
+                if (j <= n - i) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
