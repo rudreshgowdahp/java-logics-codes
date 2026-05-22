4831c89406411d256b0e35cbece67004eb26ec72
rudresh <rudreshgowdahp@gmail.com>
2026-05-22 20:45:16 +0530

    update pattern printing  in java

--- palindrome/src/com/thinksworks/palindrome/external/NumberTriangluar.java
+++ palindrome/src/com/thinksworks/palindrome/external/NumberTriangluar.java
@@ -2,7 +2,7 @@ package com.thinksworks.palindrome.external;
 
 public class NumberTriangluar {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 5;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n + i - 1; j++) {
                 if (j <= n - i) {
